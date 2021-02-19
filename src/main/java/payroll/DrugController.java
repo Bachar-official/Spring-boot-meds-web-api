205fe6a0c29d1f6d4d34a96d3c54210efa867fda
ibacharnikov <ibacharnikov@cinimex.ru>
2021-02-19 14:40:39 +0500

    search by query

--- src/main/java/payroll/DrugController.java
+++ src/main/java/payroll/DrugController.java
@@ -2,16 +2,22 @@ package payroll;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 class DrugController {
+
+    @Autowired
     private final DrugRepository repository;
 
     DrugController(DrugRepository repository) {
@@ -25,6 +31,13 @@ class DrugController {
         return repository.findAll();
     }
     // end::get-aggregate-root[]
+
+    @GetMapping("/api/DrugsApi/search")
+    List<Drug> allByQuery(
+        @RequestParam(value = "query") String query
+    ) {
+        return repository.findByQuery(query.toUpperCase());
+    }
 
     @PostMapping("/api/DrugsApi")
     Drug newDrug(@RequestBody Drug newDrug) {
--- src/main/java/payroll/DrugRepository.java
+++ src/main/java/payroll/DrugRepository.java
@@ -1,7 +1,16 @@
 package payroll;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 interface DrugRepository extends JpaRepository<Drug, Long> {
     
+    @Query(value = "SELECT * FROM drug WHERE UPPER(name) LIKE %:query%" + 
+    " OR UPPER(action) LIKE %:query%" +
+    " OR UPPER(location) LIKE %:query%" +
+    " OR expired LIKE %:query%", nativeQuery = true)
+    List<Drug> findByQuery(@Param("query")String query);
 }
